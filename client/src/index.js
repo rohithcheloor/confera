0abba88ae03f97dddf01b5b117038e739ab18719
Rohith Cheloor <rohithcm101@gmail.com>
2023-10-17 22:33:38 -0400

    Revamped Login page, Connected Backend with Login page

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./assets/css/landing.css";
-import "./assets/css/Room.css";
-import "./assets/css/VideoGrid.css";
 
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
