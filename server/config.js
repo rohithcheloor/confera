4015c95b651bd56b02c4a95785ad314faeab3e6a
Veera Sudhan <60940029+veerasudhan98@users.noreply.github.com>
2023-11-01 23:06:44 -0400

    sprint 3 changes

--- server/config.js
+++ server/config.js
@@ -1,16 +1,16 @@
-'use strict';
-const config =  {
-    server: {
-        listen: {
-            // app listen on
-            ip: '0.0.0.0',
-            port: process.env.PORT || 3010,
-        },
-        ssl: {
-            // ssl/README.md
-            cert: './cert/cert.pem',
-            key: './cert/key.pem',
-        },
+"use strict";
+const config = {
+  server: {
+    listen: {
+      // app listen on
+      ip: "0.0.0.0",
+      port: process.env.PORT || 5001,
     },
+    ssl: {
+      // ssl/README.md
+      cert: "./cert/cert.pem",
+      key: "./cert/key.pem",
+    },
+  },
 };
 export default config;
