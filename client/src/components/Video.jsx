ab67dd703dbdcd61bc3b0ccd238dedc875ac0657
Rohith Cheloor <rohithcm101@gmail.com>
2023-11-07 19:13:56 -0500

    Conference update

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,13 +1,30 @@
 import React, { useEffect, useRef } from "react";
+import { toast } from "react-toastify";
+import { createPosterImage } from "../utilities/imageMaker";
 
 const VideoTile = (props) => {
   const { peer, index, peerName, videoPoster } = props;
   const ref = useRef();
   useEffect(() => {
+    const posterImage = createPosterImage(
+      String(peerName).slice(0, 2),
+      200,
+      140
+    );
+    if (ref.current) ref.current.poster = posterImage;
+  }, [peerName]);
+
+  useEffect(() => {
     peer.on("stream", (stream) => {
+      toast.success("Stream Started");
       if (ref.current) ref.current.srcObject = stream;
     });
   }, [peer]);
+  useEffect(() => {
+    peer.on("close", () => {
+      toast.error("Stream Closed");
+    });
+  });
   return (
     <div className="video-tile-container">
       <video
