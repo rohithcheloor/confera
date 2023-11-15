de8c30c39423880ac1967349e930a37ee66aa125
Maria-Ann <mariashjauann@gmail.com>
2023-11-14 22:13:41 -0500

    Chat Client

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -23,14 +23,14 @@ const Chat = (props) => {
         if(!message || !message.username || !message.time || !message.text){
             return;
         }
-        const div = document.createElement('div');
+        const div     = document.createElement('div');
         div.classList.add('message');
-        const p = document.createElement('p');
+        const p       = document.createElement('p');
         p.classList.add('meta');
-        p.innerText = message.username;
+        p.innerText   = message.username;
         p.innerHTML += `<span> ${message.time}</span>`;
         div.appendChild(p);
-        const para = document.createElement('p');
+        const para    = document.createElement('p');
         para.classList.add('text');
         para.innerText = message.text;
         div.appendChild(para);
